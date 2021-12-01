5ed19dd02838fb38f3ee96614f36aad40b396e94
Antoni Spaanderman <56turtle56@gmail.com>
2021-12-01 19:26:07 +0100

    add template

---
+++ template.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         {NAME}
+// @namespace    https://github.com/antonilol/userscripts
+// @version      1.0.0
+// @description  Description
+// @author       antonilol
+// @updateURL    https://raw.githubusercontent.com/antonilol/userscripts/master/{NAME}.meta.js
+// @downloadURL  https://raw.githubusercontent.com/antonilol/userscripts/master/{NAME}.user.js
+// @match        https://www.example.com/
+// @icon         https://www.example.com/favicon.ico
+// @grant        none
+// ==/UserScript==
