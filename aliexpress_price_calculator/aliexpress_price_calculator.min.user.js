4390a05af7d40a6a63b661241087847c091a29f8
Antoni Spaanderman <56turtle56@gmail.com>
2022-03-14 17:46:36 +0100

    update uglify + fix download url

--- aliexpress_price_calculator/aliexpress_price_calculator.min.user.js
+++ aliexpress_price_calculator/aliexpress_price_calculator.min.user.js
@@ -4,8 +4,8 @@
 // @version      1.0.0
 // @description  Calculates price including VAT, excluding VAT, shipping per item, etc
 // @author       antonilol
-// @updateURL    https://raw.githubusercontent.com/antonilol/userscripts/master/aliexpress_price_calculator.min.meta.js
-// @downloadURL  https://raw.githubusercontent.com/antonilol/userscripts/master/aliexpress_price_calculator.min.user.js
+// @updateURL    https://raw.githubusercontent.com/antonilol/userscripts/master/aliexpress_price_calculator/aliexpress_price_calculator.min.meta.js
+// @downloadURL  https://raw.githubusercontent.com/antonilol/userscripts/master/aliexpress_price_calculator/aliexpress_price_calculator.min.user.js
 // @match        https://*.aliexpress.com/item/*
 // @icon         https://aliexpress.com/favicon.ico
 // @grant        none
